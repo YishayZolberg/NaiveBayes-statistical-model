c5f19e8a64bb3cb239dbedc7012d70a10398803e
YishayZolberg <yishayzolberg@gmail.com>
2022-09-08 19:10:50 +0300

    read db and create lists

--- driver.py
+++ driver.py
@@ -1,1 +1,13 @@
-#import db file and put in 2 lists
+import csv
+TITLES = []
+LOCAL_DB=[]
+
+with open('db.csv') as db:
+    reader = csv.reader(db, delimiter=',')
+    for row in reader:
+        LOCAL_DB.append(row)
+TITLES=LOCAL_DB[0].pop()
+print(LOCAL_DB)
+print(TITLES)
+
+
