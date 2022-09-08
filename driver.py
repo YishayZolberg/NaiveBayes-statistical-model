be33162429c46dc123a9559f46536924a50907ce
OmriBenHur <omribhk2000@gmail.com>
2022-09-08 21:05:11 +0300

    omri end day commit for thursday

--- driver.py
+++ driver.py
@@ -1,13 +1,24 @@
 import csv
-TITLES = []
-LOCAL_DB=[]
 
-with open('db.csv') as db:
+titles = []
+local_db = []
+
+with open('agaricus-lepiota_update.csv') as db:
     reader = csv.reader(db, delimiter=',')
     for row in reader:
-        LOCAL_DB.append(row)
-TITLES=LOCAL_DB[0].pop()
-print(LOCAL_DB)
-print(TITLES)
+        local_db.append(row)
+titles = tuple(local_db.pop(0))
+unique_values = {}
+for i in range(len(local_db)):
+    unique_values[i] = local_dby[i][3]
+print(set(unique_values.values()))
 
+
+#for i in range(len(local_db)):
+ #   for values in titles:
+  #      unique_values[values] = set(unique_values[j][0] for j in range(len(local_db)))
+    #if local_db[i][-1] == 'yes':
+     #   yes += 1
+    #elif local_db[i][-1] == 'no':
+     #   no += 1
 
