4e0ea18497464a88e435ccf4c871c8f61335061b
dmitriyshub <d.shtranvasser@gmail.com>
2022-09-10 01:16:50 +0300

    adding standart deviation func

--- Version1/calculator.py
+++ Version1/calculator.py
@@ -1,3 +1,4 @@
+from math import sqrt
 def mean(numbers):
 	"""
     Calculate the mean of a list of numbers
@@ -6,3 +7,12 @@ def mean(numbers):
 	"""
 	return sum(numbers)/float(len(numbers))
 
+def standart_deviation(numbers):
+	"""
+	Calculate the standard deviation of a list of numbers
+	:param numbers:
+	:return:
+	"""
+	avg = mean(numbers)
+	variance = sum([(x-avg)**2 for x in numbers]) / float(len(numbers)-1)
+	return sqrt(variance)
