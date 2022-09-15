cf2855c17fa7c1ddcf240a705549575520d1fd3f
YishayZolberg <yishayzolberg@gmail.com>
2022-09-15 17:44:06 +0300

    first commit

---
+++ hadar.py
@@ -0,0 +1,23 @@
+
+
+
+
+
+dic = {'age': {'middle_age': {'yes': 0, 'no': 20}, 'senior': {'yes': 3, 'no': 2}, 'youth': {'yes': 2, 'no': 3}},
+'income': {'high': {'yes': 2, 'no': 22}, 'medium':{'yes': 4, 'no': 2}, 'low':{'yes': 3, 'no': 1}},
+'student': {'yes': {'yes': 0, 'no': 1}, 'no': {'yes': 18, 'no': 5}}}
+
+userDic = ['middle_age','high','no']
+
+def yishay_calc(dic, userDic):
+    buy = 0.0
+    not_buy = 0.0
+    templen = 0
+    for title, uniq in zip(dic,userDic):
+        templen = dic[title][uniq]['yes']+dic[title][uniq]['no']
+        buy = dic[title][uniq]['yes']/templen
+        not_buy = dic[title][uniq]['no']/templen
+    print(buy)
+    print(not_buy)
+
+print(yishay_calc(dic, userDic))
