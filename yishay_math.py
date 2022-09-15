bc2da5d1264c1e29b1705b6f4cd62832dd4ca013
YishayZolberg <yishayzolberg@gmail.com>
2022-09-15 20:41:46 +0300

    Final17 mathgit add . 15.19

--- yishay_math.py
+++ yishay_math.py
@@ -3,7 +3,7 @@
 
 
 
-dic = {'age': {'middle_age': {'yes': 0, 'no': 20}, 'senior': {'yes': 3, 'no': 2}, 'youth': {'yes': 2, 'no': 3}},
+dic = {'age': {'middle_age': {'yes': 5, 'no': 4}, 'senior': {'yes': 3, 'no': 2}, 'youth': {'yes': 2, 'no': 3}},
 'income': {'high': {'yes': 2, 'no': 22}, 'medium':{'yes': 4, 'no': 2}, 'low':{'yes': 3, 'no': 1}},
 'student': {'yes': {'yes': 0, 'no': 1}, 'no': {'yes': 18, 'no': 5}}}
 
@@ -15,9 +15,14 @@ def yishay_calc(dic, userDic):
     templen = 0
     for title, uniq in zip(dic,userDic):
         templen = dic[title][uniq]['yes']+dic[title][uniq]['no']
-        buy = dic[title][uniq]['yes']/templen
-        not_buy = dic[title][uniq]['no']/templen
+        if buy == 0.0:
+            buy = dic[title][uniq]['yes'] / templen
+            not_buy = dic[title][uniq]['no'] / templen
+        else:
+            buy *= dic[title][uniq]['yes']/templen
+            not_buy *= dic[title][uniq]['no']/templen
     print(buy)
     print(not_buy)
+    return buy > not_buy
 
 print(yishay_calc(dic, userDic))
