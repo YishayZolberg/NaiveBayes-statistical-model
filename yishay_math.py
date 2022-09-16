b24b65e7f89c1a23ee8daf2a8f7dd9fd6a49d49b
YishayZolberg <yishayzolberg@gmail.com>
2022-09-16 09:08:41 +0300

    fix my fun if some value eq 0

--- yishay_math.py
+++ yishay_math.py
@@ -3,7 +3,7 @@
 
 
 
-dic = {'age': {'middle_age': {'yes': 5, 'no': 4}, 'senior': {'yes': 3, 'no': 2}, 'youth': {'yes': 2, 'no': 3}},
+dic = {'age': {'middle_age': {'yes': 0, 'no': 4}, 'senior': {'yes': 3, 'no': 2}, 'youth': {'yes': 2, 'no': 3}},
 'income': {'high': {'yes': 2, 'no': 22}, 'medium':{'yes': 4, 'no': 2}, 'low':{'yes': 3, 'no': 1}},
 'student': {'yes': {'yes': 0, 'no': 1}, 'no': {'yes': 18, 'no': 5}}}
 
@@ -13,16 +13,29 @@ def yishay_calc(dic, userDic):
     buy = 0.0
     not_buy = 0.0
     templen = 0
+    yes = 0
+    no = 0
     for title, uniq in zip(dic,userDic):
         templen = dic[title][uniq]['yes']+dic[title][uniq]['no']
+        yes = dic[title][uniq]['yes']
+        no = dic[title][uniq]['no']
+        if yes == 0:
+            templen += 1
+            yes += 1
+        if no == 0:
+            templen += 1
+            no += 1
+        print(templen, yes, no)
         if buy == 0.0:
-            buy = dic[title][uniq]['yes'] / templen
-            not_buy = dic[title][uniq]['no'] / templen
+            buy = yes / templen
         else:
-            buy *= dic[title][uniq]['yes']/templen
-            not_buy *= dic[title][uniq]['no']/templen
-    print(buy)
-    print(not_buy)
+            buy *= yes / templen
+
+        if not_buy == 0.0:
+            not_buy = no / templen
+        else:
+            not_buy *= no / templen
+
     return buy > not_buy
 
 print(yishay_calc(dic, userDic))
