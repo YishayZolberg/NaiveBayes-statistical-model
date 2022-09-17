e7cf8674c600fb9bc1963372a24d3ae91ce6b233
dmitriyshub <d.shtranvasser@gmail.com>
2022-09-17 21:20:30 +0300

    main file update 2

--- Version3/main.py
+++ Version3/main.py
@@ -2,8 +2,8 @@ from unique_vals import *
 import csv
 
 unique_vals, dict_corl = class_main()
-
-
+print(unique_vals)
+print(dict_corl)
 def divide_file_70_30(reader):
     local_db70 = []
     local_db30 = []
@@ -37,10 +37,10 @@ def calc(dic, userDic):
         yes = dic[title][uniq]['yes']
         no = dic[title][uniq]['no']
         if yes == 0:
-            templen += len(dic[title].keys())
+            templen += 1
             yes += 1
         if no == 0:
-            templen += len(dic[title].keys())
+            templen += 1
             no += 1
         #print(templen, yes, no)
         if buy == 0.0:
@@ -54,9 +54,10 @@ def calc(dic, userDic):
             not_buy *= no / templen
 
     # print(f'buy {buy} , \nnot buy {not_buy}')
-    buy *= dict_corl['yes'] / sum(dict_corl.values())
-    not_buy *= dict_corl['no'] / sum(dict_corl.values())
-    # print(f'buy {buy} , \nnot buy {not_buy}')
+    buy = buy * (dict_corl['yes'] / sum(dict_corl.values()))
+    not_buy = not_buy * (dict_corl['no'] / sum(dict_corl.values()))
+    #print(f'buy {buy} , \nnot buy {not_buy}')
+
     return 1 if buy > not_buy else 0
 
 def accuracy(y_true,y_pred):
@@ -76,8 +77,6 @@ def check_probability(local_db30):
     for row in local_db30:
         list_of_ans.append(calc(unique_vals,row[:-1]))
         buy_comp.append(row[-1])
-    print(buy_comp)
-    print(list_of_ans)
 
     for i in range(len(buy_comp)):
 
@@ -85,18 +84,17 @@ def check_probability(local_db30):
             buy_comp_final.append(1)
         else:
             buy_comp_final.append(0)
-    print(buy_comp_final)
     return accuracy(buy_comp_final,list_of_ans)
 
 def user_input(titles):
     answers = []
     print(f'All Dataset Titles: {titles}')
     for title in titles:
-        answers.append(input(f"Please answer according to - {title}: {unique_vals[title].keys()}\t"))
-    print(answers)
+        answers.append(input(f"Please answer according to - {title}: {unique_vals[title].keys()}:\t"))
     return answers
 
 def main():
+    print("Naive Bayes Classificator")
 
     with open('db.csv') as db:
         reader = csv.reader(db, delimiter=',')
@@ -107,8 +105,12 @@ def main():
     # for i in titles:
     #     print(unique_vals[i].keys())
     answer = user_input(titles)
-    print(calc(unique_vals,answer))
-    print("Naive Bayes Classificator")
+    if (calc(unique_vals,answer)) == 1:
+        print('YES!')
+    elif (calc(unique_vals,answer)) == 0:
+        print('NO!')
+
+    #print(calc(unique_vals,answer))
 
 
 if __name__ == '__main__':
